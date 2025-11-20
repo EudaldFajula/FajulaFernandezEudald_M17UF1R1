686ac4c000fc9b4ecf87b1a38d1a04704a29263e
Eudald Fajula <eudald.fajula.7e6@itb.cat>
2025-11-20 23:29:00 +0100

    Arreglar animacion torreta y hacer pantalla final

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -1,37 +1,50 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
-    private float TimeBetweenBullets = 1f;
-    private float TimeSpan = 0f;
+    [SerializeField] private float timeBetweenBullets = 1f;
     [SerializeField] private GameObject bullet;
-    private float timeInterval;
     [SerializeField] private Vector2 initialPosition;
     [SerializeField] private Animator animator;
 
+    private float nextFireTime = 0f;
     public Stack<GameObject> BulletStack = new Stack<GameObject>();
 
-    // Update is called once per frame
     void Update()
     {
-        if (Time.time >= TimeSpan)
+        if (Time.time >= nextFireTime)
         {
-            if (BulletStack.Count == 0)
-            {
-                GameObject bull = Instantiate(bullet, initialPosition, Quaternion.identity);
-                bull.GetComponent<Bullet>()._spawner = this;
-            }
-            else
-            {
-                GameObject bull = BulletStack.Pop();
-                Debug.Log("Reusing enemy from stack");
-                //bull.SetActive(true);
-                //bull.transform.position = transform.position;
-            }
-
-            TimeSpan = Time.time + TimeBetweenBullets;
+            Fire();
+            nextFireTime = Time.time + timeBetweenBullets;
         }
-        animator.SetBool("Shoot", BulletStack.Count == 0);
+    }
+
+    void Fire()
+    {
+        animator.SetTrigger("Shoot"); // Activar animación de disparo
+
+        GameObject bull;
+        if (BulletStack.Count == 0)
+        {
+            bull = Instantiate(bullet, initialPosition, Quaternion.identity);
+            bull.GetComponent<Bullet>()._spawner = this;
+        }
+        else
+        {
+            bull = BulletStack.Pop();
+            Debug.Log("Reusing bullet from stack");
+            // bull.SetActive(true);
+            // bull.transform.position = initialPosition;
+        }
+
+        StartCoroutine(ResetToIdle());
+    }
+
+    IEnumerator ResetToIdle()
+    {
+        yield return new WaitForSeconds(nextFireTime); // Ajusta según la duración de la animación "Shoot"
+        animator.Play("Idle");
     }
 }
